22ab41abdd9d8e165e315fd4c75261f9d4e8316e
Abhishek jadhav <109812422+Abhishekjadhavv@users.noreply.github.com>
2022-10-01 08:39:43 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# competitiveCodingQuestion-3
+# Peak Index in a Mountain Array
+
+### Problem Statement:
+
+An array arr a mountain if the following properties hold:
+- arr.length >= 3
+
+There exists some i with 0 < i < arr.length - 1 such that:
+- arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+- arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+Given a mountain array arr , return the index i such that arr[0] < arr[1] < ... < arr[i -
+1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1] .
+
+## Example 1:
+
+- Input: arr = [0,1,0]
+- Output: 1
+
+## Example 2:
+
+- Input: arr = [0,10,5,2]
+- Output: 1
+
---
+++ peakIndex.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[8], len = 0, size = 8, check = 1, index;
+
+    cout << "Enter 8 element" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+        len += 1;
+    }
+
+    if (len <= 2)
+    {
+        cout << "This is not a mountain Array" << endl;
+        return 0;
+    }
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (arr[i] < arr[i + 1])
+        {
+            if (check == 0)
+            {
+                cout << "This is not a mountain Array" << endl;
+                return 0;
+            }
+            check = 1;
+        }
+        else if (arr[i] > arr[i + 1])
+        {
+            if (check == 1)
+                index = i;
+            check = 0;
+        }
+    }
+
+    cout << "Index is " << index << endl;
+    return 0;
+}
